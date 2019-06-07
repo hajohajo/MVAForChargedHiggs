7348427d1e5edb1262b4b64417fef65e29f2ef31
hajohajo <joona.havukainen@helsinki.fi>
2019-06-07 15:04:34 +0200

    Initial Commit

---
+++ MVATrainerForChargedHiggs/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  MVATrainerForChargedHiggs
+//
+//  Created by Joona Havukainen on 6/7/19.
+//  Copyright © 2019 Joona Havukainen. All rights reserved.
+//
+
+#include <iostream>
+#include <TTree.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
